0169d92fff7c834f457d36e42671e38ccf67958a
Jared Henderson <jared@netrivet.com>
2020-09-29 15:46:43 -0400

    handle local node_module location when installed normally

--- src/dev.js
+++ src/dev.js
@@ -4,7 +4,7 @@ const { spawnSync } = require(`child_process`);
 const path = require(`path`);
 const fs = require(`fs`);
 
-const NODE_MODULES = path.resolve(__dirname, `..`, `node_modules`);
+const localModules = resolveNodeModulesPath();
 const cwd = process.cwd();
 const args = process.argv.slice(2);
 const script = args.shift();
@@ -12,7 +12,7 @@ const script = args.shift();
 if (script.startsWith(`test`)) {
   ensureJestConfig(cwd);
   const nodeArgs = [
-    `${NODE_MODULES}/.bin/jest`,
+    `${localModules}/.bin/jest`,
     args.shift() || `.`,
     ...(script === `test:watch` ? [`--watch`] : []),
   ];
@@ -21,7 +21,7 @@ if (script.startsWith(`test`)) {
 
 if (script.startsWith(`lint`)) {
   const nodeArgs = [
-    `${NODE_MODULES}/.bin/eslint`,
+    `${localModules}/.bin/eslint`,
     `--config`,
     `${__dirname}/../.eslintrc.js`,
     `**/*.{ts,tsx,js}`,
@@ -32,7 +32,7 @@ if (script.startsWith(`lint`)) {
 
 if (script.startsWith(`format`)) {
   const nodeArgs = [
-    `${NODE_MODULES}/.bin/prettier`,
+    `${localModules}/.bin/prettier`,
     `--config`,
     `${__dirname}/../.prettierrc.json`,
     `**/*.{ts,tsx,js,css,yml}`,
@@ -42,12 +42,15 @@ if (script.startsWith(`format`)) {
 }
 
 if (script === `ts:check`) {
-  spawnSync(`${NODE_MODULES}/.bin/tsc`, [`--noEmit`, `-p`, `.`], { stdio: `inherit` });
+  spawnSync(`${localModules}/.bin/tsc`, [`--noEmit`, `-p`, cwd], {
+    stdio: `inherit`,
+    cwd,
+  });
 }
 
 /**
  * @param {string} cwd
- * @returns void
+ * @returns {void}
  */
 function ensureJestConfig(cwd) {
   const destPath = `${cwd}/jest.config.js`;
@@ -59,7 +62,20 @@ function ensureJestConfig(cwd) {
     destPath,
     `// auto-generated, do not edit\n${config.replace(
       `<abs-path-to-ts-jest>`,
-      `${NODE_MODULES}/ts-jest/dist/`,
+      `${localModules}/ts-jest/dist/`,
     )}`,
   );
+}
+
+/**
+ * @returns {string}
+ */
+function resolveNodeModulesPath() {
+  let nmPath = path.resolve(__dirname, `..`, `..`, `..`);
+  // happy path, this is installed as a normal npm module
+  if (nmPath.endsWith(`node_modules`)) {
+    return nmPath;
+  }
+  // this takes over if we're `npm link`-ed during dev
+  return path.resolve(__dirname, `..`, `node_modules`);
 }
